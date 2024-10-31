41f4ed1d45a334c5818df9a8451934c1fa52a770
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-10-31 22:02:23 +0500

    start main page

--- src/components/AppRoute.jsx
+++ src/components/AppRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Main from '../pages/Main'
+import Chat from '../pages/chat'
 
 export default function AppRoute() {
     return (
         <>
             <Routes>
                 <Route path='/' element={<Main />} />
+                <Route path='/chat' element={<Chat />} />
             </Routes>
         </>
     )
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from  '../assets/css/main.module.cssnb  '
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className={styles.wrap}>
+<div className={styles.container}>
+  <h1 className={styles.heading}>Join</h1>
+</div>
+    </div>
   )
 }
